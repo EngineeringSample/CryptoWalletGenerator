8c1df9e5d935816efe2d3000074f2ad4fcabbc35
YuanCh <126393524+YuanLiuchang@users.noreply.github.com>
2023-10-16 21:04:10 +0800

    Update CryptoWalletGenerator.sh

--- CryptoWalletGenerator.sh
+++ CryptoWalletGenerator.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Check if Python is installed
-if ! command -v python &> /dev/null; then
-    echo "Python is not installed. Please install Python."
-    exit 1
-fi
-
-# Check if pip is installed
-if ! command -v pip &> /dev/null; then
-    echo "pip is not installed. Please install pip."
-    exit 1
-fi
-
 # Install Python dependencies
 pip install eth-account bitcoinlib tronapi
 
